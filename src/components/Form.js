2b91af000985cf2d83f22a2b1c6758d1c35fa6ae
Michael Muenzer <michael.muenzer@gmail.com>
2021-04-22 22:28:04 +0200

    Simplify bookmarks list

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,16 +5,13 @@ import {
   Input,
   Loader,
   Dimmer,
-  Segment,
   Divider,
   Grid,
-  Label,
 } from 'semantic-ui-react';
 
 const ContentForm = (props) => {
   return (
     <>
-      <Segment>
         <Dimmer active={props.loading}>
           <Loader active={props.loading} />
         </Dimmer>
@@ -27,58 +24,42 @@ const ContentForm = (props) => {
                   <Form.Field>
                     <label>Your Bookmarks</label>
                   </Form.Field>
-                  <Form.Group inline>
-                    <Button
-                      icon
-                      labelPosition='left'
-                      variant="success"
-                      disabled={props.loggedIn !== true || !props.filePath}
-                      onClick={(e) => {
-                        props.loadData(e);
-                      }}
-                    >
-                      <Icon name='refresh' />
-                      Refresh
-                    </Button>
-                  </Form.Group>
                   <Form.Field>
-                    <div>
-                      {props.bookmarks.map((bookmark) => (
-                        <Grid key={bookmark.lastUpdatedTimestamp} columns='equal'>
-                          <Grid.Column width={4}>
-                            <Form.Input
-                              placeholder={bookmark.bookmarkName}
-                              onChange={(e) => {
-                                props.updateBookmark(e.target.value, bookmark)
-                              }}
-                            />
-                          </Grid.Column>
-                          <Grid.Column width={9}>
-                            <a href={bookmark.bookmarkUrl} target="_blank">{bookmark.bookmarkUrl}</a>
-                          </Grid.Column>
-                          <Grid.Column width={1}>
-                            <Button
-                              icon
-                              style={{ marginLeft: '20px' }}
-                              variant="success"
-                              disabled={
-                                props.loggedIn !== true ||
-                                !props.filePath
-                              }
-                              onClick={(e) => {
-                                props.handleSaveAndRecord(e, bookmark);
-                              }}
-                            >
-                              <Icon name='save' />
-                            </Button>
-                          </Grid.Column>
-                        </Grid>
-                      ))}
-                    </div>
+                    {props.bookmarks.map((bookmark) => (
+                      <Grid key={bookmark.lastUpdatedTimestamp} columns='equal'>
+                        <Grid.Column width={4}>
+                          <Form.Input
+                            placeholder={bookmark.bookmarkName}
+                            onChange={(e) => {
+                              props.updateBookmark(e.target.value, bookmark)
+                            }}
+                          />
+                        </Grid.Column>
+                        <Grid.Column width={9}>
+                          <a href={bookmark.bookmarkUrl} target="_blank">{bookmark.bookmarkUrl}</a>
+                        </Grid.Column>
+                        <Grid.Column width={1}>
+                          <Button
+                            icon
+                            style={{ marginLeft: '20px' }}
+                            variant="success"
+                            disabled={
+                              props.loggedIn !== true ||
+                              !props.filePath
+                            }
+                            onClick={(e) => {
+                              props.handleSaveAndRecord(e, bookmark);
+                            }}
+                          >
+                            <Icon name='save' />
+                          </Button>
+                        </Grid.Column>
+                      </Grid>
+                    ))}
                   </Form.Field>
                   <Divider />
                   <Form.Field>
-                      <label>New Bookmark</label>
+                      <label>Add Bookmark</label>
                     </Form.Field>
                   <Form.Group>
                     <Form.Input
@@ -95,13 +76,10 @@ const ContentForm = (props) => {
                         props.setBookmarkUrl(e.target.value);
                       }}
                     />
-                  </Form.Group>
-                  <Button primary type="submit">
+                    <Button primary type="submit">
                     Add
-                  </Button>
-                  <Label pointing="left" basic color="green">
-                    Store public bookmark associated to MySky.
-                  </Label>
+                    </Button>
+                  </Form.Group>
                 </Form>
             </>
             )}
@@ -147,7 +125,6 @@ const ContentForm = (props) => {
             )}
           </>                
         )}
-      </Segment>
     </>
   );
 };
